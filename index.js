3acf7b3fc173dc230ffece819e65dc097a13d4ae
Jonathas <jonathass5678@gmail.com>
2024-03-16 12:11:54 -0300

    servertechfix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use( bodyParser.urlencoded({ extended: true }) );
 require('dotenv').config()
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173/")
+  res.header("Access-Control-Allow-Origin", "https://techfixfr.vercel.app/")
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
   app.use(cors())
   next()
