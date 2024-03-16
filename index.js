3d5750a242385b4dab0ca4e9a5da2b848b56af09
Jonathas <jonathass5678@gmail.com>
2024-03-16 13:03:12 -0300

    servertechfix

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const bodyParser = require('body-parser');
 import cors from 'cors';
 const ConnectBD = require('./server/connectionDB')
 const Order = require('./models/pedido');
+const corsConfig = require('./cors.config')
 
 const app = express();
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
 require('dotenv').config()
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://techfixfr.vercel.app/")
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-  app.use(cors())
-  next()
-} )
+corsConfig
 
 app.get('/content/v1/orders', async (req, res) => {
     try {
